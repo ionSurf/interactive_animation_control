4132009185c7d8d52880d4f4f5dece4ef1a1d5bc
Javier <javier@Jessicas-MacBook-Pro.local>
2020-05-13 17:34:09 -0400

    wrapped up into a real shell

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,27 @@
-python3 -m http.server 3002 & 
-open -a /Applications/Google\ Chrome.app http://localhost:3002/index3.html
+#!/bin/bash
+
+helpFunction() {
+   echo ""
+   echo "Usage: $0 -p port"
+   echo -e "\t-p Defines the port to use for the temporary web server"
+   exit 1 # Exit script after printing help
+}
+
+#while getopts "a:b:c:" opt
+while getopts "p:" opt
+do
+   case "$opt" in
+      p ) port="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$port" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+#echo "$port"
+open -a /Applications/Google\ Chrome.app http://localhost:$port/ & python3 -m http.server $port
